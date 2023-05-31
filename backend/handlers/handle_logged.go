aec30ee066d00714921808c810a7f35bdc15cf98
Tauno Tammeoja <tauno.tammeoja@gmail.com>
2023-05-31 04:58:03 +0300

    rtf

--- backend/handlers/handle_logged.go
+++ backend/handlers/handle_logged.go
@@ -46,34 +46,5 @@ func LoggedHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "500 internal server error", http.StatusInternalServerError)
 		return
 	}
-
-	// var user *UserRecord
-	// user = foorum_dao.find_user_record_for_session(sessionID)
-	// if my_post == "on" && nil != user {
-
-	// 	filter.CreatorUserId = user.Id
-	// }
-
-	// my_like := r.FormValue("mylike")                  //3mai
-	// fmt.Println("dashboard handle minulaik", my_like) //3mai
-
-	// if my_like == "on" && nil != user { //3mai
-
-	// 	filter.LikedByUserId = user.Id //3mai
-	// 	fmt.Println("handledashboard filterliked", filter.LikedByUserId)
-
-	// }
-
-	// filter.Category = strings.Fields(r.FormValue("category"))
-
-	// dashboardData := DashboardData{}
-	// if nil != user {
-	// 	dashboardData.LoggedIn = true
-	// 	dashboardData.FirstName = user.FirstName
-	// 	dashboardData.LastName = user.LastName
-	// }
-	// dashboardData.Posts = foorum_dao.list_posts(filter)
-	// dashboardData.Filter = filter
-	// tpl.ExecuteTemplate(w, "dashboard.html", dashboardData)
-
+	
 }
--- clientfrontend/src/components/LoginPage.vue
+++ clientfrontend/src/components/LoginPage.vue
@@ -1,67 +1,66 @@
 <template>
-    <div>
-      <form @submit.prevent="handleSubmit">
-        <h3>Login</h3>
-        <span class="error badge bg-secondary" v-if="error">
-          Incorrect Nickname/Email or Password
-        </span>
-        <div>
-          <label for="username">Nickname/Email:</label>
-          <input type="text" id="username" v-model="username" required />
-        </div>
-        <div>
-          <label for="password">Password:</label>
-          <input type="password" id="password" v-model="password" required />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <router-link to="register" class="nav-link">Sign up</router-link>
-    </div>
-  </template>
-  <script>
-  import axios from 'axios';
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <h3>Login</h3>
+      <span class="error badge bg-secondary" v-if="error">
+        Incorrect Nickname/Email or Password
+      </span>
+      <div>
+        <label for="username">Nickname/Email:</label>
+        <input type="text" id="username" v-model="username" required />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+    <router-link to="/register" class="nav-link">Sign up</router-link>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      error: false,      
+    };
+  },
   
-  export default {
-    data() {
-      return {
-        username: '',
-        password: '',
-        error: false
-      };
-    },
-    methods: {
-      async handleSubmit() {
-        try {
-          const formData = {
-            username: this.username,
-            password: this.password
-          };
-          console.log(JSON.stringify(formData))
-          const response = await axios.post('http://localhost:8082/login',
-           JSON.stringify(formData));
-  
-          if (response.data.success) {
-            // handle success response
-            this.handleSuccessResponse(response);
-          } else {
-            this.error = true;
-          }
-        } catch (error) {
-          // handle the error
-        }
-      },
-      handleSuccessResponse(response) {
-        // handle success response
-        // Store the user data in the local storage
-        localStorage.setItem("sessionID", response.data.sessionID);
-        // e.g. redirect the user to the dashboard or home page
-        console.log(response)
-       console.log('joudsin siia')
-        //this.$router.push({ name: '/loggedUser' });
-        this.$router.push(this.response.data.success);
+  methods: {
+    async handleSubmit() {
+      try {
+        const formData = {
+          username: this.username,
+          password: this.password
+        };
+        console.log('joyson', JSON.stringify(formData))
+        const response = await axios.post('http://localhost:8082/login', JSON.stringify(formData));
         
-        console.log('joudsin siia peale routerit')
-      },
+        if (response.data.success) {          
+            this.handleSuccessResponse(response);
+            console.log('joyson2', JSON.stringify(formData))
+          
+        } else {
+          this.error = true;
+        }
+      } catch (error) {
+        // Handle the error
+      }
     },
-  };
-  </script>
+    handleSuccessResponse(response) {
+      localStorage.setItem('sessionID', response.data.sessionID);
+      console.log(response)
+      console.log('joudsin siia')
+
+      this.$router.push('/loggedUser' );
+      console.log('joudsin siia peale routerit')
+    }
+  }
+};
+</script>
